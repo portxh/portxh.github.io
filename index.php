0f1dc44f6c1b8109bffba2d24b8098a818215305
Porto <75947688+portxh@users.noreply.github.com>
2023-03-24 20:52:50 -0300

    portxh

--- index.php
+++ index.php
@@ -53,37 +53,13 @@
         
 
             <!-- Titulo da Seção -->
-                <h1>Ofertas da Semana</h1>            
+            <h1>Ofertas da Semana</h1>            
 
-            <!-- Conjunto de Cards/Produtos -->
-            <div class="card-deck d-flex justify-content-around">
+            <?php
 
-                <!-- Card de UM Produto -->
-                <?php
+                include 'cardProd.php';
 
-                // Obtendo os produtos do banco de dados
-                $resultado = mysqli_query($con, "SELECT * FROM produtos");
-
-                // Exibindo os produtos na vitrine
-                while ($IdProd = mysqli_fetch_array($resultado)) {
-                    echo '<div class="card">';
-                        
-                        echo '<img src="img/' . $IdProd["imagem"] . '" class="img-prod" alt="' . $IdProd["nomeProd"] . '">';
-                        
-                        echo '<div class="card-body">';
-                            echo '<h5 class="card-title">' . $IdProd["nomeProd"] . '</h5>';
-                            echo '<p class="card-text">' . $IdProd["descProd"] . '</p>';
-                            echo '<p class="card-text">R$ ' . number_format($IdProd["precoProd"], 2, ',', '.') . '</p>';
-                        echo '</div>';
-                        //echo '<div class="card-footer"';
-                            echo '<button type="submit" class="btn justify-content-around btn-primary">Adicionar ao Carrinho</button>';
-                        //echo '</div>';
-                    echo '</div>';
-                }
-
-                ?>
-               
-            </div>
+            ?>
 
         </div>
     </div>
