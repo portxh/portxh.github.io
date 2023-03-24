7cf1c2067257b951f6921348530cca9b89975082
Porto <75947688+portxh@users.noreply.github.com>
2023-03-24 20:21:12 -0300

    teste

---
+++ navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#inicio">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#inicio">Inicio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#ofertas">Ofertas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#loja">Bolos e Doces</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+            Usuario
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#signin-modal.php">Entrar</a>
+          <a class="dropdown-item" href="#signup-modal.php">Cadastrar</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
