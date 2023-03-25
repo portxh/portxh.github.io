e16c95a7fd3148e5dd89a2a2919cbcf288b0c862
Porto <75947688+portxh@users.noreply.github.com>
2023-03-24 21:08:57 -0300

    portxh

--- cardProd.php
+++ cardProd.php
@@ -10,19 +10,19 @@
     // Exibindo os produtos na vitrine
     while ($IdProd = mysqli_fetch_array($resultado)) {
         echo '<div class="card-deck">';
-        echo '<div class="card-4">';
-            
-            echo '<img src="img/' . $IdProd["imagem"] . '" class="img-prod" alt="' . $IdProd["nomeProd"] . '">';
-            
-            echo '<div class="card-body">';
-                echo '<h5 class="card-title">' . $IdProd["nomeProd"] . '</h5>';
-                echo '<p class="card-text">' . $IdProd["descProd"] . '</p>';
-                echo '<p class="card-text">R$ ' . number_format($IdProd["precoProd"], 2, ',', '.') . '</p>';
+            echo '<div class="card-4">';
+                
+                echo '<img src="img/' . $IdProd["imagem"] . '" class="img-prod" alt="' . $IdProd["nomeProd"] . '">';
+                
+                echo '<div class="card-body">';
+                    echo '<h5 class="card-title">' . $IdProd["nomeProd"] . '</h5>';
+                    echo '<p class="card-text">' . $IdProd["descProd"] . '</p>';
+                    echo '<p class="card-text">R$ ' . number_format($IdProd["precoProd"], 2, ',', '.') . '</p>';
+                echo '</div>';
+                //echo '<div class="card-footer"';
+                    echo '<button type="submit" class="btn justify-content-around btn-primary">Adicionar ao Carrinho</button>';
+                //echo '</div>';
             echo '</div>';
-            //echo '<div class="card-footer"';
-                echo '<button type="submit" class="btn justify-content-around btn-primary">Adicionar ao Carrinho</button>';
-            //echo '</div>';
-        echo '</div>';
         echo '</div>';
 
     }
